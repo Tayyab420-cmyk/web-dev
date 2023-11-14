d8628385643f8147317797cc2bda7b0413ce3f8a
Tayyab Ahmad <ahmadtayyab047@gmail.com>
2023-11-14 09:14:18 +0500

    class task

---
+++ 14.11.23/second.js
@@ -0,0 +1,51 @@
+// var result;
+// console.log("Enter the number");
+// if(result>50){
+//     console.log("congratulation you are passed");
+
+// }else{
+//     console.log("sorry,you failed");
+// }
+// var food="normal";
+// if (food=="hot") {
+//     console.log("I like hot food");
+// }
+// else if(food == "cold"){
+// console.log("too cold");
+// }
+// else{
+//     console.log('unknown type of food');
+// }
+// var place="first";
+// switch(place){
+//     case 'first':
+//         console.log("this is first floor");
+//         break;
+//         case 'second':
+//             console.log("this is second floor");
+//             break;
+//             case 'third':
+//                 console.log("this is third floor");
+//                 break;
+//                 case 'fourth':
+//                     console.log("this is fourth floor");
+//                     break;
+//                     default:
+//                         console.log("unknown floor");
+// }
+var age=65;
+if(age>=65)
+{
+    console.log("You get your income from pension");
+
+}
+else if(age<65 && age>18){
+    console.log("Each month you get a salary");
+
+}
+else if(age<18){
+    console.log("You get an allowance");
+}
+else{
+console.log("the value is nor numerical");
+}
---
+++ 7.11.23/myfirst.js
@@ -0,0 +1,32 @@
+console.log("hello world");
+var greeting="Hello";
+console.log(greeting,"tayyab");
+console.log(typeof ("greet"));
+console.log("15");
+console.log(typeof (2));
+console.log(typeof (true));
+var a;
+console.log(typeof (a));
+console.log(2+5);
+var a=7;
+var b=5;
+console.log(a<b);
+var currenttime=20;
+console.log(currenttime>9&&currenttime<17);
+console.log(currenttime>9||currenttime<17);
+console.log(22/5);
+console.log(typeof(4.4));
+var pethungry=true;
+pethungry=!pethungry;
+console.log(pethungry);
+console.log(5==5);
+console.log(5===5);
+console.log(5==="5");
+var overtime=1;
+console.log(overtime+=3);
+console.log(overtime+=3);
+console.log(overtime-=5);
+var longstring=
+
+
+
